138ed11ccf288578a838d43c3d408a3b772dda7d
sussa3007 <sussa1933@gmail.com>
2023-04-09 20:47:07 +0900

    feat: 각 파라미터별 검색 기능 구현

--- src/main/java/com/sideproject/conopli/repository/impl/TjTjMusicRepositoryImpl.java
+++ src/main/java/com/sideproject/conopli/repository/impl/TjTjMusicRepositoryImpl.java
@@ -1,16 +1,32 @@
 package com.sideproject.conopli.repository.impl;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPQLQuery;
 import com.sideproject.conopli.constant.ErrorCode;
+import com.sideproject.conopli.constant.MusicNation;
+import com.sideproject.conopli.constant.SearchType;
 import com.sideproject.conopli.exception.ServiceLogicException;
+import com.sideproject.conopli.music.dto.MusicDto;
+import com.sideproject.conopli.music.dto.MusicQueryDto;
+import com.sideproject.conopli.music.entity.QTjMusic;
 import com.sideproject.conopli.music.entity.TjMusic;
 import com.sideproject.conopli.repository.TjMusicJpaRepository;
 import com.sideproject.conopli.repository.TjMusicRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-@RequiredArgsConstructor
-public class TjTjMusicRepositoryImpl implements TjMusicRepository {
+public class TjTjMusicRepositoryImpl extends QuerydslRepositorySupport implements TjMusicRepository {
+    public TjTjMusicRepositoryImpl(TjMusicJpaRepository jpaRepository) {
+        super(TjMusic.class);
+        this.jpaRepository = jpaRepository;
+    }
     private final TjMusicJpaRepository jpaRepository;
 
     @Override
@@ -26,9 +42,88 @@ public class TjTjMusicRepositoryImpl implements TjMusicRepository {
 
     }
 
+    @Override
+    public TjMusic findTjMusicById(Long musicId) {
+        return verifiedMusicById(musicId);
+    }
+
+    @Override
+    public TjMusic findTjMusicByNum(String num) {
+        return verifiedMusic(num);
+    }
+
+    @Override
+    public Page<MusicQueryDto> findQueryMusic(
+            MusicNation nation,
+            SearchType searchType,
+            String keyWord,
+            Pageable pageable
+    ) {
+        QTjMusic tjMusic = QTjMusic.tjMusic;
+        JPQLQuery<MusicQueryDto> query = from(tjMusic)
+                .select(
+                        Projections.constructor(
+                                MusicQueryDto.class,
+                                tjMusic.musicId,
+                                tjMusic.num,
+                                tjMusic.title,
+                                tjMusic.singer,
+                                tjMusic.lyricist,
+                                tjMusic.composer,
+                                tjMusic.youtubeUrl,
+                                tjMusic.nation
+                        ));
+        if (nation != null) {
+            query.where(
+                    tjMusic.nation.eq(nation)
+            );
+        }
+        if (searchType != null) {
+            if (searchType.equals(SearchType.NUM)) {
+                query.where(
+                        tjMusic.num.containsIgnoreCase(keyWord)
+                );
+            } else if (searchType.equals(SearchType.SINGER)) {
+                query.where(
+                        tjMusic.singer.containsIgnoreCase(keyWord)
+                );
+            } else if (searchType.equals(SearchType.LYRICIST)) {
+                query.where(
+                        tjMusic.lyricist.containsIgnoreCase(keyWord)
+                );
+            } else if (searchType.equals(SearchType.COMPOSER)) {
+                query.where(
+                        tjMusic.composer.containsIgnoreCase(keyWord)
+                );
+            } else if (searchType.equals(SearchType.TITLE)) {
+                query.where(
+                        tjMusic.title.containsIgnoreCase(keyWord)
+                );
+            }
+        }
+        List<MusicQueryDto> musicList = Optional.ofNullable(getQuerydsl())
+                .orElseThrow(() -> new ServiceLogicException(
+                        ErrorCode.DATA_ACCESS_ERROR))
+                .applyPagination(pageable, query)
+                .fetch();
+        return new PageImpl<>(musicList, pageable, query.fetchCount());
+    }
+
     private void verifyMusic(String num) {
         if (jpaRepository.findByNum(num).isPresent()) {
             throw new ServiceLogicException(ErrorCode.EXISTS_MUSIC_NUM);
         }
+    }
+    private TjMusic verifiedMusic(String num) {
+        return jpaRepository.findByNum(num)
+                .orElseThrow(
+                        () -> new ServiceLogicException(ErrorCode.NOT_FOUND_MUSIC)
+                );
+    }
+    private TjMusic verifiedMusicById(Long musicId) {
+        return jpaRepository.findById(musicId)
+                .orElseThrow(
+                        () -> new ServiceLogicException(ErrorCode.NOT_FOUND_MUSIC)
+                );
     }
 }
