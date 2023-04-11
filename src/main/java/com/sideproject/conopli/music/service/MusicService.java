cadaa0789c9dc425a6df3a154e9aa2d39f4ad4f2
sussa3007 <sussa1933@gmail.com>
2023-04-11 23:14:24 +0900

    feat: MusicService 로직 구현 및 스캐쥴러 메소드 작성

--- src/main/java/com/sideproject/conopli/music/service/MusicService.java
+++ src/main/java/com/sideproject/conopli/music/service/MusicService.java
@@ -3,20 +3,30 @@ package com.sideproject.conopli.music.service;
 import com.sideproject.conopli.constant.MusicNation;
 import com.sideproject.conopli.constant.SearchType;
 import com.sideproject.conopli.dto.PageResponseDto;
+import com.sideproject.conopli.dto.ResponseDto;
+import com.sideproject.conopli.music.dto.MusicDto;
 import com.sideproject.conopli.music.dto.MusicQueryDto;
+import com.sideproject.conopli.music.dto.PopularRequestDto;
 import com.sideproject.conopli.repository.TjMusicRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class MusicService {
 
     private final TjMusicRepository tjMusicRepository;
+
+    private final CrawlingService crawlingService;
 
     public PageResponseDto searchMusic(
             MusicNation nation,
@@ -26,6 +36,26 @@ public class MusicService {
     ) {
         Page<MusicQueryDto> queryMusic = tjMusicRepository.findQueryMusic(nation, searchType, keyWord, pageable);
         return PageResponseDto.of(queryMusic.getContent(), queryMusic);
+    }
+
+    public ResponseDto searchNewMusic() {
+        return crawlingService.getNewMusicCrawling();
+    }
+
+    public ResponseDto searchPopularMusic(PopularRequestDto requestDto) {
+        return crawlingService.getPopularCrawling(requestDto);
+    }
+
+
+    @Scheduled(cron = "0 0 05 * * *")
+    public void saveNewSong() {
+        log.info("@@ Start SaveNewSong Task!");
+        ResponseDto newMusicCrawling = crawlingService.getNewMusicCrawling();
+        List<MusicDto> newMusic = (List<MusicDto>) newMusicCrawling.getData();
+        newMusic.forEach(music -> crawlingService.createMusicCrawling(music.getNum(), MusicNation.KOR));
+        newMusic.forEach(music -> crawlingService.createMusicCrawling(music.getNum(), MusicNation.JPN));
+        newMusic.forEach(music -> crawlingService.createMusicCrawling(music.getNum(), MusicNation.ENG));
+        log.info("@@ Finish SaveNewSong Task!");
     }
 
 }
