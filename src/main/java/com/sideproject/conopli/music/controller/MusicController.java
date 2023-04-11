f9e454f2f748a46fdf73729de089fcd83c1e44ca
sussa3007 <sussa1933@gmail.com>
2023-04-11 23:15:16 +0900

    chore: MusicController MusicService 의존성 추가

--- src/main/java/com/sideproject/conopli/music/controller/MusicController.java
+++ src/main/java/com/sideproject/conopli/music/controller/MusicController.java
@@ -1,6 +1,7 @@
 package com.sideproject.conopli.music.controller;
 
 
+import com.sideproject.conopli.music.service.MusicService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,4 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 public class MusicController {
+
+    private final MusicService musicService;
+
+
 }
